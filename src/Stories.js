55c0388d40a867a0c33bb84b7f66b72aaafddafd
dharathakkarz <dharat@zignuts.com>
2024-04-15 17:47:09 +0530

    likebtn

--- src/Stories.js
+++ src/Stories.js
@@ -1,51 +1,57 @@
-import React, { useEffect } from 'react'
-import { useGlobalContext } from './context'
+
+import React, { useState } from 'react';
+import { useGlobalContext } from './context';
 
 const Stories = () => {
+    const { hits, nbPages, isLoading, removeNews } = useGlobalContext();
 
-    const { hits, nbPages, isLoading , removeNews } = useGlobalContext();
     if (isLoading) {
         return (
-            <>
-                <h1>Loading...</h1>
-            </>
+            <h1>Loading...</h1>
         )
-
     }
 
-
-
-
     return (
-        <>
-       
         <div className='stories-div'>
-        
             {hits.map((curNews) => {
                 const { title, author, objectID, url, num_comments } = curNews;
-                return (
-                    <>
-                    <div className='card' key={objectID}>
-
-                     <h2>{title}</h2>
-                  
-                    <p>
-                        By <strong>{author}</strong> | <strong>{num_comments}</strong> comments
-                   
-                    </p>
-                    <div className='card-button'>
-                    <a href={url}target='_blank' >Read more</a>
-                    <a href='#' onClick={()=>removeNews(objectID)}>Remove</a>
-                    </div>
-                    </div>
-                    </>
-                )
-
+                return <StoryCard key={objectID} {...{ title, author, objectID, url, num_comments, removeNews }} />;
             })}
-
         </div>
-        </>
-    )
+    );
 }
 
-export default Stories
+const StoryCard = ({ title, author, objectID, url, num_comments, removeNews }) => {
+    const [showMessage, setShowMessage] = useState(false);
+
+    const handlePress = () => {
+        setShowMessage(true);
+        setTimeout(() => {
+            setShowMessage(false);
+        }, 3000); 
+    };
+
+    return (
+        <div className='card'>
+            <h2>{title}</h2>
+            <p>
+                By <strong>{author}</strong> | <strong>{num_comments}</strong> comments
+            </p>
+            <div className='card-button'>
+                <a href={url} target='_blank' rel="noopener noreferrer">Read more</a>
+                <a href='#' onClick={() => removeNews(objectID)}>Remove</a>
+            </div>
+            <div>
+                <button
+                    className="big"
+                    onClick={handlePress}
+                >
+                    🍕
+                </button>
+                {showMessage && <p style={{ color:'red' }} > Thanks for your like!! Pizza is the best</p>}
+            </div>
+        </div>
+    );
+}
+
+export default Stories;
